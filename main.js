733879e60dc80f5e0d03aa2cdd016f8b3da64a1b
Cristian Rosca <cristianrrosca@gmail.com>
2022-06-10 18:16:27 +0100

    project setup complete

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+
+const serverUrl = "https://o3hhzsfj1trr.usemoralis.com:2053/server";
+      const appId = "nGXhWCPzyinol9YPoiH2FHexUBVQHetmSYhAUYNW";
+      Moralis.start({ serverUrl, appId });
+
+
